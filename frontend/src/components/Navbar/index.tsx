4a3f48fe82bb2ccdd5434821a70423870fe7c3b5
singgih budi hartono <singgihbudih@gmail.com>
2024-07-27 14:57:22 +0700

    in progress

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,36 @@
+import Solid from "@/assets/solid.svg";
+
+function Navbar() {
+    return (
+        <nav class="px-[50px]">
+            {" "}
+            <div class="navbar bg-base-100">
+                <div class="flex-1">
+                    <a class="btn btn-ghost text-xl">daisyUI</a>
+                </div>
+                <div class="flex-none">
+                    <ul class="menu menu-horizontal px-1">
+                        <li>
+                            <a>Link</a>
+                        </li>
+                        <li>
+                            <details>
+                                <summary>Parent</summary>
+                                <ul class="bg-base-100 rounded-t-none p-2">
+                                    <li>
+                                        <a>About</a>
+                                    </li>
+                                    <li>
+                                        <a>Features</a>
+                                    </li>
+                                </ul>
+                            </details>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,165 @@
 import Solid from "@/assets/solid.svg";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-function index() {
+function Home() {
     return (
         <div>
-            <img src={Solid} alt="" />
+            <Navbar />
+            {/* hero */}
+            <div class="hero bg-base-200 min-h-screen">
+                <div class="hero-content flex-col lg:flex-row-reverse">
+                    <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-5xl font-bold">
+                            Mudah, Transparant, <br /> and Aman
+                        </h1>
+                        <p class="py-6">Adukan keluhan anda dengan anonimus dan rahasia</p>
+                        <div class="flex flex-row gap-2">
+                            {" "}
+                            <button class="btn btn-primary">About</button>
+                            <button class="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* techstack */}
+            <div class="carousel carousel-center rounded-box">
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Pizza" />
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" alt="Pizza" />
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" alt="Pizza" />
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" alt="Pizza" />
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="Pizza" />
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="Pizza" />
+                </div>
+                <div class="carousel-item">
+                    <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" alt="Pizza" />
+                </div>
+            </div>
+            {/* About */}
+            <div class="flex flex-row p-10 items-center justify-between">
+                {" "}
+                <div class="card bg-base-100 image-full w-96 shadow-xl">
+                    <figure>
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card bg-base-100 image-full w-96 shadow-xl">
+                    <figure>
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card bg-base-100 image-full w-96 shadow-xl">
+                    <figure>
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Form */}
+            <div class="relative flex flex-col justify-center h-screen overflow-hidden">
+                <div class="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+                    <h1 class="text-3xl font-semibold text-center text-purple-700">DaisyUI</h1>
+                    <form class="space-y-4">
+                        <div>
+                            <label class="label">
+                                <span class="text-base label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="Name" class="w-full input input-bordered input-primary" />
+                        </div>
+                        <div>
+                            <label class="label">
+                                <span class="text-base label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="Email Address" class="w-full input input-bordered input-primary" />
+                        </div>
+                        <div>
+                            <label class="label">
+                                <span class="text-base label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="Enter Password" class="w-full input input-bordered input-primary" />
+                        </div>
+                        <div>
+                            <label class="label">
+                                <span class="text-base label-text">Confirm Password</span>
+                            </label>
+                            <input type="password" placeholder="Confirm Password" class="w-full input input-bordered input-primary" />
+                        </div>
+                        <div>
+                            <button class="btn btn-block btn-primary">Sign Up</button>
+                        </div>
+                        <span>
+                            Already have an account ?
+                            <a href="#" class="text-blue-600 hover:text-blue-800 hover:underline">
+                                Login
+                            </a>
+                        </span>
+                    </form>
+                </div>
+            </div>
+            {/* end form */}
+            {/* FAQ */}
+            <div class="join join-vertical w-full">
+                <div class="collapse collapse-arrow join-item border-base-300 border">
+                    <input type="radio" name="my-accordion-4" checked="checked" />
+                    <div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div class="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div class="collapse collapse-arrow join-item border-base-300 border">
+                    <input type="radio" name="my-accordion-4" />
+                    <div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div class="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div class="collapse collapse-arrow join-item border-base-300 border">
+                    <input type="radio" name="my-accordion-4" />
+                    <div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div class="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+            {/* end FAQ */}
+            {/* Footer */}
+            <Footer />
+            {/* Footer */}
         </div>
     );
 }
 
-export default index;
+export default Home;

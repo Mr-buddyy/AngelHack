87fcd9dc719137d4cb2470c6ac5c86220af05e43
singgih budi hartono <singgihbudih@gmail.com>
2024-07-27 15:39:58 +0700

    in progress

---
+++ frontend/src/assets/index.tsx
@@ -0,0 +1,5 @@
+import Python from "./Icons/Python.png";
+import MySQL from "./Icons/MySQL.png";
+import Solidjs from "./Icons/Solidjs.png";
+import TailwindCSS from "./Icons/TailwindCSS.png";
+export { TailwindCSS, Solidjs, MySQL, Python };
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import Solid from "@/assets/solid.svg";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { TailwindCSS, Solidjs, MySQL, Python } from "@/assets";
 
 function Home() {
     return (
@@ -24,27 +25,18 @@ function Home() {
                 </div>
             </div>
             {/* techstack */}
-            <div class="carousel carousel-center rounded-box">
+            <div class="flex flex-row justify-between p-10 bg-black">
                 <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Pizza" />
+                    <img src={TailwindCSS} alt="Pizza" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp" alt="Pizza" />
+                    <img src={Solidjs} alt="Pizza" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp" alt="Pizza" />
+                    <img src={MySQL} alt="Pizza" />
                 </div>
                 <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp" alt="Pizza" />
-                </div>
-                <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp" alt="Pizza" />
-                </div>
-                <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp" alt="Pizza" />
-                </div>
-                <div class="carousel-item">
-                    <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp" alt="Pizza" />
+                    <img src={Python} alt="Pizza" />
                 </div>
             </div>
             {/* About */}

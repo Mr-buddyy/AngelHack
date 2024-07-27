09c4366fac790e1d57990e934342e47f156d4461
Nazar Alwi <alwinazar75@gmail.com>
2024-07-27 16:21:26 +0700

    Improve footer

--- frontend/src/assets/index.tsx
+++ frontend/src/assets/index.tsx
@@ -2,4 +2,5 @@ import Python from "./Icons/Python.png";
 import MySQL from "./Icons/MySQL.png";
 import Solidjs from "./Icons/Solidjs.png";
 import TailwindCSS from "./Icons/TailwindCSS.png";
-export { TailwindCSS, Solidjs, MySQL, Python };
+import Logo from "./Icons/Logo.png";
+export { TailwindCSS, Solidjs, MySQL, Python, Logo };
